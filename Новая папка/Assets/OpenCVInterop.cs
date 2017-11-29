0c32ac280141820f6b38854a9b631f4260dab61f
nikgoust <nikgoust@gmail.com>
2017-11-29 19:03:36 +0300

    test
    123

---
+++ Новая папка/Assets/OpenCVInterop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+internal static class OpenCVInterop
+{
+    [DllImport("OpenCvUnity")]
+    internal static extern int Init(ref int outCameraWidth, ref int outCameraHeight);
+
+    [DllImport("OpenCvUnity")]
+    internal static extern int Close();
+
+    [DllImport("OpenCvUnity")]
+    internal static extern int SetScale(int downscale);
+
+    [DllImport("OpenCvUnity")]
+    internal static extern unsafe void Detect(CvCircle* outFaces, int maxOutFacesCount, ref int outDetectedFacesCount);
+}
+
+
+[StructLayout(LayoutKind.Sequential, Size = 12)]
+public struct CvCircle
+{
+    public int X, Y, Radius;
+}
